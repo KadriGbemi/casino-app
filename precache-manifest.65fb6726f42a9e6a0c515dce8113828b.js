4139b2a597f122ba274f29939ec126ef4a73a263
kadri <kge.sportcompass.dk>
2019-11-22 00:43:43 +0200

    Updates

---
+++ precache-manifest.65fb6726f42a9e6a0c515dce8113828b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f5322e0192eb326738f71efa072185",
+    "url": "/casino-app/index.html"
+  },
+  {
+    "revision": "fd74e8186912c34b3a2b",
+    "url": "/casino-app/static/css/main.8138b5a0.chunk.css"
+  },
+  {
+    "revision": "586ae2bcf33ad5e388f0",
+    "url": "/casino-app/static/js/2.77fd36af.chunk.js"
+  },
+  {
+    "revision": "fd74e8186912c34b3a2b",
+    "url": "/casino-app/static/js/main.bfdef191.chunk.js"
+  },
+  {
+    "revision": "7dea6d42b1cf1f224d28",
+    "url": "/casino-app/static/js/runtime-main.dd4960ed.js"
+  },
+  {
+    "revision": "84e1352a120482d9b1548a91ef71a652",
+    "url": "/casino-app/static/media/bg.84e1352a.png"
+  },
+  {
+    "revision": "50a6406b14318918118039d917c4504d",
+    "url": "/casino-app/static/media/figure.50a6406b.png"
+  },
+  {
+    "revision": "64c89343a7f0d3cf05cad440a4a28f47",
+    "url": "/casino-app/static/media/icon.64c89343.svg"
+  },
+  {
+    "revision": "c308b16344499236f418118827a04f58",
+    "url": "/casino-app/static/media/logo.c308b163.svg"
+  }
+]);
