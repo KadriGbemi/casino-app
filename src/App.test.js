19dee7f9b346eb8a9e04441faafb51fdce95ab41
kadri <kge.sportcompass.dk>
2019-11-24 23:47:37 +0200

    Full dom rendering UI testing with Enzyme, Chai and SinoDn

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,59 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme from 'enzyme';
+import { expect } from 'chai';
+import Enzyme, { mount } from 'enzyme';
+import sinon from 'sinon';
+import Adapter from 'enzyme-adapter-react-16';
 
 import App from './App';
+import GetCryptoPriceList from './components/list/getCryptoPrice';
+import TextInputFormComponent from './components/form/textInput';
 
-describe("'Unit testing of BitCasino's (User Interface) components and it's properties'", () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+import Fab from '@material-ui/core/Fab';
+import IconButton from '@material-ui/core/IconButton';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("'App components and it's sub-components'", () => {
+  it("'renders the App component and it's sub-components'", () => {
+    const AppComponentWrapper = mount(<App />);
+    const TextInputFormWrapper = mount(<TextInputFormComponent />);
+    const GetCryptoPriceListWrapper = mount(<GetCryptoPriceList />);
+    expect(AppComponentWrapper).to.have.lengthOf(1);
+    expect(TextInputFormWrapper).to.have.lengthOf(1);
+    expect(GetCryptoPriceListWrapper).to.have.lengthOf(1);
+  });
+  it('proves that App component calls ComponentDidMount;', () => {
+    sinon.spy(App.prototype, 'componentDidMount');
+    const wrapper = mount(<App />);
+    expect(App.prototype.componentDidMount).to.have.property('callCount', 1);
+  });
+  it('proves that GetCryptoPriceList component calls ComponentDidMount;', () => {
+    sinon.spy(GetCryptoPriceList.prototype, 'componentDidMount');
+    const wrapper = mount(<GetCryptoPriceList />);
+    expect(GetCryptoPriceList.prototype.componentDidMount).to.have.property(
+      'callCount',
+      1
+    );
+  });
+  it('proves that App component calls componentDidUpdate;', () => {
+    sinon.spy(App.prototype, 'componentDidUpdate');
+    const wrapper = mount(<App />);
+    expect(App.prototype.componentDidUpdate).to.have.property('callCount', 1);
+  });
+  it('proves that GetCryptoPriceList component calls componentDidUpdate;', () => {
+    sinon.spy(GetCryptoPriceList.prototype, 'componentDidUpdate');
+    const wrapper = mount(<GetCryptoPriceList />);
+    expect(GetCryptoPriceList.prototype.componentDidUpdate).to.have.property(
+      'callCount',
+      1
+    );
+  });
+  it('simulates click for the add crypto-code button in (TextInputForm) Component', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = mount(
+      <TextInputFormComponent handleAddButtonClick={onButtonClick} />
+    );
+    wrapper.find(Fab).simulate('click');
+    expect(onButtonClick).to.have.property('callCount', 1);
   });
 });
